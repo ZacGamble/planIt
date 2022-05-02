fd6ba9307fdf4f996e1281ad8e9aaea618f8c16a
Joshua Wagstaff-Leon <wagstaffleon.joshua@gmail.com>
2022-05-02 12:11:34 -0600

    fixed typo

--- planIt/server/models/Sprint.js
+++ planIt/server/models/Sprint.js
@@ -5,7 +5,7 @@ export const SprintSchema = new Schema(
     {
         name: { type: String, required: true },
         projectId: { type: Schema.Types.ObjectId, ref: "Project", required: true },
-        creatorId: { Type: Schema.Types.ObjectId, ref: "Account", required: true }
+        creatorId: { type: Schema.Types.ObjectId, ref: "Account", required: true }
     },
     {
         timestamps: true,
