21f91e41fa9833a87020c2655ef00f43d5196ff7
Joshua Wagstaff-Leon <wagstaffleon.joshua@gmail.com>
2022-05-02 14:00:59 -0600

    note service and schema

--- planIt/server/db/DbContext.js
+++ planIt/server/db/DbContext.js
@@ -4,6 +4,7 @@ import { ValueSchema } from '../models/Value'
 import { ProjectSchema } from '../models/Project.js';
 import { SprintSchema } from '../models/Sprint.js';
 import { TaskSchema } from '../models/Task.js';
+import { NoteSchema } from '../models/Note.js';
 
 class DbContext {
   Values = mongoose.model('Value', ValueSchema);
@@ -12,6 +13,7 @@ class DbContext {
   Projects = mongoose.model('Project', ProjectSchema);
   Sprints = mongoose.model('Sprint', SprintSchema);
   Tasks = mongoose.model('Task', TaskSchema);
+  Notes = mongoose.model('Note', NoteSchema);
 }
 
 export const dbContext = new DbContext()
