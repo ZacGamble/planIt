3427929a9ea55f2e3306306003a279b6dd6acac2
ZacGamble <zacrgamble@gmail.com>
2022-05-02 12:23:46 -0600

    sprint tests passing

--- planIt/server/controllers/SprintsController.js
+++ planIt/server/controllers/SprintsController.js
@@ -4,23 +4,24 @@ import BaseController from "../utils/BaseController";
 
 export class SprintsController extends BaseController {
     constructor(){
-        super('api/projects/:projectId/sprints')
+        super('api/projects')
         this.router
         .use(Auth0Provider.getAuthorizedUserInfo)
-        .get('', this.getAll)
-        .post('', this.create)
-        .delete('/:sprintId', this.remove)
+        .get('/:projectId/sprints', this.getAll)
+        .post('/:projectId/sprints', this.create)
+        .delete('/:projectId/sprints/:sprintId', this.remove)
     }
    async getAll(req, res, next) {
 try {
-    const sprints = await sprintsService.getAll(req.userInfo.id)
+    const sprints = await sprintsService.getAll(req.params.projectId)
     return res.send(sprints)
 } catch (error) {
     next(error)
 }    }
    async create(req, res, next) {
 try {
-    req.body.creatorId = req.userInfo.userId
+    req.body.creatorId = req.userInfo.id
+    req.body.projectId = req.params.projectId
     const sprint = await sprintsService.create(req.body)
     return res.send(sprint)
 } catch (error) {
@@ -28,7 +29,7 @@ try {
 }    }
    async remove(req, res, next) {
 try {
-    const removedSprint = await sprintsService.remove(req.params.id, req.userInfo.id)
+    const removedSprint = await sprintsService.remove(req.params.sprintId, req.userInfo.id)
     return res.send(removedSprint)
     
 } catch (error) {
