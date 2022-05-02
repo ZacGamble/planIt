b9732fdf37f3e73bc97180d835ccad4bb224f267
ZacGamble <zacrgamble@gmail.com>
2022-05-02 16:20:40 -0600

    project page and sprint component

---
+++ planIt.client/src/components/Sprint.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="row">
+    <div class="col-md-6">Title, weight</div>
+    <div class="col-md-6">Task completion and add task form button</div>
+  </div>
+  <div class="row">
+    <div class="col">
+      <!-- The tasks and their associated comments will go here NOTE -->
+    </div>
+  </div>
+</template>
+
+
+<script>
+export default {
+  setup() {
+    return {}
+  }
+}
+</script>
+
+
+<style lang="scss" scoped>
+</style>
---
+++ planIt.client/src/pages/ProjectPage.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-6"></div>
+      <div class="col-md-6"></div>
+    </div>
+    <Sprint v-for="s in sprints" :key="s.id" :sprint="s" />
+  </div>
+</template>
+
+
+<script>
+import { computed } from '@vue/reactivity'
+import Sprint from '../components/Sprint.vue'
+import { AppState } from '../AppState'
+export default {
+  components: { Sprint },
+  setup() {
+    return {
+      sprints: computed(() => AppState.sprints)
+    }
+  }
+}
+</script>
+
+
+<style lang="scss" scoped>
+</style>
