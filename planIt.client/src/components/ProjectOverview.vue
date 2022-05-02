56669c85667d156af3e80bf0f8403106335ec120
Joshua Wagstaff-Leon <wagstaffleon.joshua@gmail.com>
2022-05-02 15:15:36 -0600

    project overview computed populating

--- planIt.client/src/components/ProjectOverview.vue
+++ planIt.client/src/components/ProjectOverview.vue
@@ -1,14 +1,23 @@
 <template>
-    <div class="row mb-4" v-for="p in 5" :key="p">
-        <div class="col-4">PROJECT NAME</div>
-        <div class="col-4">MEMBER PICTURE</div>
-        <div class="col-4">STARTED</div>
+    <div class="row mb-4">
+        <div class="col-4">{{ project.name }}</div>
+        <div class="col-4">{{ project.creator.picture }}</div>
+        <div class="col-4">{{ project.createdAt }}</div>
     </div>
 </template>
 
 <script>
 export default
 {
+    props:
+    {
+        project:
+        {
+            type: "Object",
+            required: true
+        }
+    },
+
     setup()
     {
         return {
