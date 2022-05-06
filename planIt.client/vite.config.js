6941ddc77732dd98a0290468d197ed3cbb394ca9
Joshua Wagstaff-Leon <wagstaffleon.joshua@gmail.com>
2022-05-06 09:12:35 -0600

    heroku test 2

--- planIt.client/vite.config.js
+++ planIt.client/vite.config.js
@@ -5,7 +5,7 @@ import { defineConfig } from 'vite'
 export default defineConfig({
   plugins: [vue()],
   build: {
-    outDir: '../planIt/',
+    outDir: '../planIt/client',
     sourcemap: false
   },
   server: {
